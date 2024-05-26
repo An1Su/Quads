179d3cd69d5673206ee8d310220e9eeb06322992
Ana <116589093+An1Su@users.noreply.github.com>
2024-05-26 05:12:11 -0700

    Add files via upload
    Quad functions

---
+++ quadC.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"os"
+	"piscine"
+	"strconv"
+)
+
+func main() {
+	if len(os.Args) != 3 {
+		return
+	}
+	a, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		return
+	}
+	b, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		return
+	}
+	if a <= 0 || b <= 0 {
+		return
+	}
+	piscine.QuadC(a, b)
+}
